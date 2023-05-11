0969ae0e878808f5a001293d9fc38d37a2c1a12e
bensboto <bensboto@gmail.com>
2023-05-10 18:13:10 -0600

    updated styling for project component

--- src/components/About.js
+++ src/components/About.js
@@ -3,19 +3,21 @@ import ionscion from "../images/ionscion.png";
 
 function About() {
   return (
-    <div className="flex flex-col justify-center items-center mt-12">
-      <h3 className="mb-4 mt-2">About Me</h3>
-      <img
-        src={ionscion}
-        alt="ionscion"
-        className="w-32 h-32 object-contain mb-4"
-      />
-      <p className="text-center">
-        World renowned bread-maker extraordinaire 😝, dipping his toes into the
-        digital world. This site is currently under construction, please pardon
-        any loud noises while we work through this together. Please click on the
-        images below to preview some of my work! 🔥{" "}
-      </p>
+    <div className="bg-white rounded-md shadow-md p-6 mt-12">
+      <h3 className="text-2xl font-bold text-gray-800 mb-4">About Me</h3>
+      <div className="flex flex-col items-center">
+        <img
+          src={ionscion}
+          alt="ionscion"
+          className="w-32 h-32 object-contain mb-4"
+        />
+        <p className="text-center text-gray-600">
+          World renowned bread-maker extraordinaire 😝, dipping his toes into
+          the digital world. This site is currently under construction, please
+          pardon any loud noises while we work through this together. Please
+          click on the images below to preview some of my work! 🔥{" "}
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
 export default function Project() {
+  const projects = [
+    {
+      name: "Concert Finder",
+      description: "A web app that allows users to search for concerts by artist, venue, or city.",
+      link: "https://ionscion.github.io/miniature-garbanzo",
+    },
+    {
+        name: "Weather Dashboard",
+        description: "A web app that allows users to search for weather by city.",
+        link: "https://ionscion.github.io/weather-dashboard",
+    },
+  ];
+
+  const renderedProjects = projects.map((project) => {
+    return (
+      <div className="mb-6 p-6 bg-white rounded-md shadow-md">
+        <h4 className="text-lg font-medium text-gray-800 mb-2">
+          {project.name}
+        </h4>
+        <p className="text-gray-600 mb-4">{project.description}</p>
+        <a
+          href={project.link}
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-500 hover:text-blue-600"
+        >
+          {project.link}
+        </a>
+      </div>
+    );
+  });
+
   return (
-    <div className='mt-12'>
-        <h3>Projects</h3>
+    <div className="mt-12">
+      <h3 className="text-2xl font-bold text-gray-800 mb-6 text-center">Projects</h3>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {renderedProjects}
+      </div>
     </div>
-  )
+  );
 }
