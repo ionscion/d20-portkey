19b56a3f4ea160b5058f4daca3acc5f621cea4fe
bensboto <bensboto@gmail.com>
2023-05-15 18:30:30 -0600

    added breakpoint and styling for small devices

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation} from "react-router-dom";
 
 function Navigation() {
+  let location = useLocation();
+
   const links = [
     { label: "Home", path: "/" },
     { label: "About", path: "/about" },
@@ -10,9 +12,12 @@ function Navigation() {
     { label: "Resume", path: "/resume" },
   ];
   const renderedLinks = links.map((link) => {
+   
+    const isActive = location.pathname === link.path;
+    const activeClass = isActive ? "text-blue-500" : "text-gray-500";
     return (
       <li key={link.label}>
-        <Link to={link.path} className="mb-3 blue-500 hover:text-blue-600">
+        <Link to={link.path} className={`mb-3 hover:text-blue-600 ${activeClass} text-sm`}>
           {link.label}
         </Link>
       </li>
@@ -20,7 +25,7 @@ function Navigation() {
   });
   return (
     <div>
-      <ul className="flex justify-center space-x-4">{renderedLinks}</ul>
+      <ul className="flex justify-center space-x-4 text-sm">{renderedLinks}</ul>
     </div>
   );
 }
